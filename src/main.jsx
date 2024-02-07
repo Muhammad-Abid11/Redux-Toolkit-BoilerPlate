ed913ece942697501641ba8b7b178f3c1a779213
Muhammad-Abid11 <abidkhan11845@gmail.com>
2024-02-07 12:37:18 +0500

    Redux-Sync LocalStoage , RootReducer-otherWays

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
+import { PersistGate } from 'redux-persist/integration/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Router from './config/router.jsx'
 import { Provider } from 'react-redux'
-import store from './store/index.jsx'
+import { store, persistor } from './store/index.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <App />
   <Provider store={store}>
-    <Router />
+    <PersistGate loading={null} persistor={persistor}>
+      <Router />
+    </PersistGate>
   </Provider>
 )
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,20 +1,59 @@
 //store
+
+//to-Connect-LocalStorage
+//reduxPersis library to connect localStorage
+// npm i redux-persist
+// https://www.npmjs.com/package/redux-persist
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+//to-Connect-LocalStorage
+
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import themeSlice from './themeslice'
 import userSlice from './userSlice'
+
+
+//-------------x----------when Single-Slice/Reducer
 /* 
 const store = configureStore({
     reducer: themeSlice.reducer, //yhn reducer:"storeKname".reducer,  <--`here reducer is a required keyword with themeSlice argument
 })
  */
 
-const combinedRoot = combineReducers({ //ap ye direct nichy bhi likh sakty ho
+//------------x---------CombinedReducers  (gooodPractice rootReducer ki file me combine kro)
+
+const rootReducer = combineReducers({ //ap ye direct nichy bhi likh sakty ho
     user: userSlice.reducer,
     theme: themeSlice.reducer,
 })
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-    reducer: combinedRoot//here
+    reducer: persistedReducer//here
+})
+const persistor = persistStore(store)
+
+// let store = configureStore(persistedReducer)
+export { store, persistor }
+// let store = createStore(persistedReducer)
+//------------x---------withOut-CombinedReducers We can also combineReducer 
+
+/* 
+
+const store = configureStore({
+    reducer: { //ap ye direct nichy bhi likh sakty ho
+        user: userSlice.reducer,
+        theme: themeSlice.reducer,
+    }
 })
 
 export default store
 
+*/
